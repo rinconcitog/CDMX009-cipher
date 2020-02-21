f3f771188fa82e1586ae283760f8b707b7a2b699
rinconcitog <60704930+rinconcitog@users.noreply.github.com>
2020-02-21 11:44:31 -0600

    entrega

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,30 @@
-const cipher = {
-  // ...
-};
 
+const cipher = {
+  encriptar,
+  desencriptar
+}
+
+function encriptar (texto,offset){
+  let palabraEncriptada="";
+  for (let i = 0 ; i < texto.length ; i++){
+    let mayusculas= texto.toUpperCase ();
+    let numeroEnAscii = texto.charCodeAt(i);
+    let formulaEncriptar = ((numeroEnAscii-65+parseInt(offset))%26+65);
+    palabraEncriptada += String.fromCharCode (formulaEncriptar);
+
+    return palabraEncriptada;
+
+  }
+}
+function desencriptar (palabraEncriptada,offset){
+  let palabraDecifrada='';
+  for(let i = 0; i< palabraEncriptada.length ; i++){
+    let mayusculas= palabraEncriptada.toUpperCase ();
+    let numeroEnAscii = palabraEncriptada.charCodeAt(i);
+    let formulaDesencriptar = ((numeroEnAscii+65-parseInt(offset))%26+65);
+desencriptar+=String.fromCharCode(palabraDecifrada);
+return palabraDecifrada
+
+  }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import cipher from './cipher.js';
 
+function myFunction() {
+
+  document.getElementById("contenido").innerHTML = "<h1>Introduzca una palabra para obtener una contraseña</h1> "+
+  "<form>"+
+  "<label for='texto'>introduzca la palabra:</label><br>"+
+  "<input id='texto' type='text'class='campo'name='palabra'><br>"+
+  "<label for='offset'>introduzca el número a recorrer:</label><br>"+
+  "<input id='offset' type='number'min='1' max='100'class='campo'name='numero'> <br> "+
+  "<button id='botonEncriptar'class='botones' onclick='myFunction2()'>Encriptar</button>"
+
+
+
+
+
+}
+function myFunction2() {
+
+  document.getElementById("contenido").innerHTML = "<h1>Contraseña Obtenida</h1> "+
+
+  "</form>"+
+  "<button class='botones' onclick='myFunction3()'>Ir a desencriptar</button>";
+
+
+
+}
+
+
+function myFunction3() {
+  document.getElementById("contenido").innerHTML = "<h1>desencriptar contraseña</h1> "+
+    "<form>"+
+    "<label for='numero'>introduzca la palabra encriptada:</label><br>"+
+    "<input id='palabra' type='text'class='campo'name='palabra'><br>"+
+    "<label for='numero'>introduzca el número a recorrer:</label><br>"+
+    "<input id='offset' type='number'class='campo'name='numero'> <br> "+
+    "</form>"+
+    "<button class='botones' onclick='myFunction4()'>desencriptar</button>";
+
+}
+    function myFunction4() {
+      document.getElementById("contenido").innerHTML = "<h1>palabra desencriptada</h1> "+
+        "<form>"+
+        "<button class='botones' onclick='myFunction5()'>Volver a inicio</button>";
+
+}
+console.log(Cipher);
+
+
 console.log(cipher);
